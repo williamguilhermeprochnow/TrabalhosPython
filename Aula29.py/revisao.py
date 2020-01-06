3e0cfac20b42add099700342cc9b2b92de9c6c0b
William Guilherme Prochnow <w.g.p6calmo@gmail.com>
2020-01-06 09:59:09 -0300

    Aula29

---
+++ Aula29.py/revisao.py
@@ -0,0 +1,14 @@
+#--- Listas
+pessoasL = ['William' , 'Maikao', 'Josue rei das listas']
+#--- Tuplas
+pessoasT = ('William' , 'Maikao', 'Josue rei das listas')
+#--- Dicionários
+pessoasD = {'nome':'William', 'professor':'Maykao', 'rei':'josue rei das listas'}
+
+print(pessoasD)
+print(pessoasT[1])
+print(pessoasL[2])
+print(pessoasD['nome'])
+
+for i in pessoasL:
+    print(i)
