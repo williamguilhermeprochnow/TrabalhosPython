cbcf43b8b6fa0c9748caf37a528dee7692eb86af
William Guilherme Prochnow <w.g.p6calmo@gmail.com>
2020-01-21 12:01:21 -0300

    Aula35

---
+++ Aula35.py/aula35.py
@@ -0,0 +1,7 @@
+#Crie uma pagina para cadastro de squads (times de desenvovlimnto)
+# 1 - Criar dao squad
+# 2 - Criar model squad
+# 3 - Criar controller squad
+# 5 - Criar view tipo console para realizar as 4 operaçoes de CRUD de squads
+# 6 - Criar View tipo Web para realizar as 4 operacoes de CRUD de Squads
+# Squad: Nome, Descricao, NumeroPessoas, LinguagemBackEnd, FrameworkFrontEnd
