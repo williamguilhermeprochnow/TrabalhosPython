5154392f8b315317faa003e030393b3a9ba2c8d7
William Guilherme Prochnow <w.g.p6calmo@gmail.com>
2020-02-11 11:43:39 -0300

    Aula35

--- Aula35/aula35.py
+++ Aula35/aula35.py
@@ -3,7 +3,7 @@
 # 2 - Criar model squad
 # 3 - Criar controller squad
 # 5 - Criar view tipo console para realizar as 4 operaçoes de CRUD de squads
-# 6 - Criar View tipo Web para realizar as 4 operacoes de CRUD de Squads
+# 6 - Criar view tipo Web para realizar as 4 operacoes de CRUD de Squads
 # Squad: Nome, Descricao, NumeroPessoas, LinguagemBackEnd, FrameworkFrontEnd
 
 #---- Parte 2
